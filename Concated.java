3bd89e8e872c914160de91b76189da8598192cf2
JenitaAngel <jenita.angelcse@ksrce.ac.in>
2016-07-25 09:48:49 +0530

    Create Concated.java

---
+++ Concated.java
@@ -0,0 +1,8 @@
+public class Concated {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("Hello");
+		StringBuffer sb1 = new StringBuffer("World");
+		System.out.println(sb.append(sb1));
+	}
+}
